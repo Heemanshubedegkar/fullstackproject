f84ec52cd7793c8709157229177cc0e34a312360
Heemanshubedegkar <136895028+Heemanshubedegkar@users.noreply.github.com>
2023-06-19 16:59:44 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,18 @@
+class Box
+{
+   private int h;
+   private int w;
+   private int d;
+
+  public void showBox()
+{
+  System.out.println(h*w*d);
+}
+  public Box(int b1)
+	{
+	   h=b1;
+	   w=b1;
+         d=b1;
+	}
+}
+
